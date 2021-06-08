0c1dd997020598cbd270218f508a6e92d2ec03f9
Iurii Chernovalov <chernovalov.iurii@gmail.com>
2021-06-08 22:03:36 +0200

    Initial Commit

---
+++ ToDos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDos
+//
+//  Created by Iurii Chernovalov on 08.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
