94ff1b8490fa62de4b53af4413dc4dde4dafec22
JuniorYurec <yurec200721@gmail.com>
2021-08-22 10:28:39 +0300

    Delete System comments

--- ToDosUITests/ToDosCategoryUITests.swift
+++ ToDosUITests/ToDosCategoryUITests.swift
@@ -10,9 +10,7 @@ import XCTest
 class ToDosCategoryUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        
         continueAfterFailure = false
     }
 
@@ -21,7 +19,6 @@ class ToDosCategoryUITests: XCTestCase {
     }
 
     func testAddCategory() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
@@ -73,7 +70,6 @@ class ToDosCategoryUITests: XCTestCase {
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
